5ab165c60b1a9347dad92148ebd3fefcbbca009e
Mariko Lee <mariko.lee@mail.utoronto.ca>
2023-06-26 23:37:29 -0400

    Update App JSX

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
     return (
         <div className="App">
-            <img src={reactLogo} alt="React Logo" width="300" />
+       UCLAX-Web1-Lee-Mariko
         </div>
     );
 }
